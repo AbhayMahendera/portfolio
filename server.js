af1b40d489230027d520c3c4ecfe75d1dbd1aa99
Abhay Mahendera <abhaymahendera0@gmail.com>
2024-02-23 13:07:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-const express = require('express');
 const path = require('path');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Set up static file serving
-app.use(express.static(path.join(__dirname, 'portfolio')));
-
-// Route for serving the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'sample.html'));
-});
 
 // Start the server
 app.listen(PORT, () => {
