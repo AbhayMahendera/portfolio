74ba4ee41eb8d3df6a738379204a27edb96c23bd
Abhay Mahendera <abhaymahendera0@gmail.com>
2024-02-23 13:11:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const express = require('express');
 const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sample.html'));
+});
+
+// Route to handle other requests
+app.get('/hello', (req, res) => {
+    res.send('Hello, World!');
+});
 
 // Start the server
 app.listen(PORT, () => {
